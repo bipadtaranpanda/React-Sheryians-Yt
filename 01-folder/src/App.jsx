8924f5f9a64900fd12a2bcbf27e4099c7485ed9d
Bipadtaran Panda <pandabipadtaran@gmail.com>
2026-09-13 02:19:03 +0530

    Initial commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,19 @@
+// rafce + ENTER
+import React from 'react'
+
+const App = () => {
+  return (
+    // <> -> this is called fregments means emoty tag or wraper
+    <>  
+      <div id='dad'>
+        <h1 id='child1'>App</h1>
+        <h3 id='child2'>Hello</h3>
+      </div>
+      <div id="chacha">
+        <h1>Hi from chacha</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
